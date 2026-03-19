c216ebddad1ad294cc1f43943b6e175ed102515f
salainiyo <salainiyo2016@gmail.com>
2026-03-19 13:59:30 +0200

    pytest fixtures

---
+++ backend/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from sqlmodel import create_engine, StaticPool, SQLModel, Session
+from fastapi.testclient import TestClient
+
+from main import app
+from database.db import get_session
+
+
+
+database_url = "sqlite://"
+engine = create_engine(url=database_url, connect_args={"check_same_thread":False}, poolclass=StaticPool)
+
+@pytest.fixture(name="session")
+def session_fixture():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+    SQLModel.metadata.drop_all(engine)
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def session_overrides():
+        return session
+    
+    app.dependency_overrides[get_session] = session_overrides
+
+    if hasattr(app.state, "limiter"):
+        app.state.limiter.enabled = False
+
+    client = TestClient(app)
+    yield client
+
+    app.dependency_overrides.clear()
