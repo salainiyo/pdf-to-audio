9ab25d97420143bd982ce4ab6e18b35074fa4b4d
salainiyo <salainiyo2016@gmail.com>
2026-03-20 15:36:29 +0200

    all tests on auth/register route

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,18 +1,22 @@
 import pytest
-from sqlmodel import create_engine, StaticPool, SQLModel, Session
+from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel.pool import StaticPool
 from fastapi.testclient import TestClient
 
+from backend.main import app
+from backend.database.db import get_session
 from backend.models.users import User
 
 database_url = "sqlite://"
-engine = create_engine(url=database_url, connect_args={"check_same_thread":False}, poolclass=StaticPool)
+engine = create_engine(url=database_url,
+                       connect_args={"check_same_thread":False},
+                       poolclass=StaticPool)
 
 @pytest.fixture(name="session")
 def session_fixture():
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
         yield session
-
     SQLModel.metadata.drop_all(engine)
 
 @pytest.fixture(name="client")
@@ -20,14 +24,7 @@ def client_fixture(session: Session):
     def session_overrides():
         return session
     
-    from backend.main import app
-    from backend.database.db import get_session
     app.dependency_overrides[get_session] = session_overrides
-
-    if hasattr(app.state, "limiter"):
-        app.state.limiter.enabled = False
-
     client = TestClient(app)
     yield client
-
     app.dependency_overrides.clear()
--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,8 +1,30 @@
 test_user = {
-    "email":"testuser@gmai.com",
+    "email":"testuser@gmail.com",
     "password":"Password@123"
 }
+invalid_email_user = {
+    "email":"testuser.gmail.com",
+    "password":"Password@123"
+}
+invalid_pwd_user = {
+    "email":"testuser@gmail.com",
+    "password":"assword@123"
+}
 
-def test_user_registration(client):
-    response = client.post("/auth/register", json=test_user)
-    assert response.status_code == 201
+class TestRegister:
+    def test_existing_user(self, client):
+        response = client.post("/auth/register", json=test_user)
+        response = client.post("/auth/register", json=test_user)
+        assert response.status_code == 400
+
+    def test_successful_registration(self, client):
+        response = client.post("/auth/register", json=test_user)
+        assert response.status_code == 201
+
+    def test_invalid_email(self, client):
+        response = client.post("/auth/register", json=invalid_email_user)
+        assert response.status_code == 422
+
+    def test_invalid_pwd(self, client):
+        response = client.post("/auth/register", json=invalid_email_user)
+        assert response.status_code ==422
